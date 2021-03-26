eb48af21061658346ad14ceaaec4592e49e61b49
klasifikasimateri <widodosejati12@gmail.com>
2021-03-26 15:20:15 +0700

    tampil

--- Awal/TampilData.js
+++ Awal/TampilData.js
@@ -11,10 +11,8 @@ var firebaseConfig = {
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
+let kuisku = document.getElementById('SelectKuis');
 function readlah() {
-    let kuisku = document.getElementById('SelectKuis');
-
     if (kuisku.value == "1") {
 
     var task = firebase.database().ref("hasil1/");
@@ -67,6 +65,7 @@ function readlah() {
                             <td class="ukr3">${taskvalue.nilai}</td>
                             <td class="ukr2">${taskvalue.hari}</td>
                             <td class="ukr2">${taskvalue.waktu}</td>
+                            <td class="hps" onclick ="hapus(${taskvalue.id})"><button type="button" class="btn btn-outline-danger">Hapus</button></td>
                         </tr>`;
             }
         });
@@ -196,7 +195,68 @@ function readlah() {
         } else {
             alert('Tentukan filter pencarian');
         }
-    } else{
+    }
+    else if(kuisku.value == "4") {
+
+        var task = firebase.database().ref("hasilA/");
+        let tmp = document.getElementById('IsiKuis');
+        tmp.innerHTML = "";
+        kelasnya = document.getElementById('SelectKelas');
+        sekolah = document.getElementById('SelectSekolah');
+        let kelasfix = '';
+        let sekolahfix = '';
+
+        if (kelasnya.value == "1") {
+            kelasfix = "7A";
+        } else if (kelasnya.value == "2") {
+            kelasfix = "7B";
+        }
+
+        if (sekolah.value == "1") {
+            sekolahfix = "SMP Mantap";
+        } else if (sekolah.value == "2") {
+            sekolahfix = "SMP Sukses";
+        }
+
+        if ((sekolahfix != '') && (kelasfix != '')) {
+            task.orderByChild("nama").on("child_added", function (data) {
+                var taskvalue = data.val();
+
+
+                if ((sekolahfix == taskvalue.sekolah) && (kelasfix == taskvalue.kelas)) {
+                    tmp.innerHTML += `<tr>
+                                <td class="ukr1">${taskvalue.sekolah}</td>
+                                <td class="ukr3">${taskvalue.kelas}</td>
+                                <td class="ukr1">${taskvalue.nama}</td>
+                                <td class="ukr3">${taskvalue.nilai}</td>
+                                <td class="ukr2">${taskvalue.hari}</td>
+                                <td class="ukr2">${taskvalue.waktu}</td>
+                            </tr>`;
+                }
+            });
+
+        } else if (sekolahfix != '') {
+            task.orderByChild("nama").on("child_added", function (data) {
+                var taskvalue = data.val();
+
+
+                if ((sekolahfix == taskvalue.sekolah)) {
+                    tmp.innerHTML += `<tr>
+                                <td class="ukr1">${taskvalue.sekolah}</td>
+                                <td class="ukr3">${taskvalue.kelas}</td>
+                                <td class="ukr1">${taskvalue.nama}</td>
+                                <td class="ukr3">${taskvalue.nilai}</td>
+                                <td class="ukr2">${taskvalue.hari}</td>
+                                <td class="ukr2">${taskvalue.waktu}</td>
+                            </tr>`;
+                }
+            });
+
+        } else {
+            alert('Tentukan filter pencarian');
+        }
+    }
+    else{
         alert('tentukan filter kuis!');
     }
 
@@ -209,7 +269,34 @@ window.onload = function () {
     sekolah.value = value = "0";
 }
 
+function hapus(id) {
+    var yakin = confirm("Apakah kamu yakin menghapus data ?");
+    if (yakin) {
+        deletetask(id);
+    } else {
 
+    }
+}
+
+function deletetask(id) {
+    if(kuisku.value == "1"){
+        var task = firebase.database().ref("hasil1/" + id);
+        task.remove();
+        readlah();
+    }else if(kuisku.value == "2"){
+        var task = firebase.database().ref("hasil2/" + id);
+        task.remove();
+        readlah();
+    }else if(kuisku.value == "3"){
+        var task = firebase.database().ref("hasil3/" + id);
+        task.remove();
+        readlah();
+    }else if(kuisku.value == "4"){
+        var task = firebase.database().ref("hasilA/" + id);
+        task.remove();
+        readlah();
+    }
+}
 
 // download data
 
